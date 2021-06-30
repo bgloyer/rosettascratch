ff1888dc9bd07e3c182e231cdf3a1c42cddca63e
brian gloyer <brian.gloyer@gmail.com>
2021-06-29 21:20:20 -0700

    claenup

--- writer-monad.cpp
+++ writer-monad.cpp
@@ -4,17 +4,12 @@
 
 using namespace std;
 
+// Use a struct as the monad
 struct LoggingMonad
 {
     double Value;
     string Log;
 };
-
-ostream& operator<<(ostream &os, const LoggingMonad &monad)
-{
-    os << monad.Log << "\nResult: " << monad.Value;
-    return os;
-}
 
 // Use the >> operator as the bind function
 auto operator>>(const LoggingMonad& monad, auto f)
@@ -23,23 +18,25 @@ auto operator>>(const LoggingMonad& monad, auto f)
     return LoggingMonad{result.Value, monad.Log + "\n" + result.Log};
 }
 
+// Define the three simple functions
 auto Root = [](double x){ return sqrt(x); };
 auto AddOne = [](double x){ return x + 1; };
 auto Half = [](double x){ return x / 2.0; };
 
+// Define a function to create writer monads from the simple functions
 auto Writer = [](auto f, string message)
 {
     return [=](double x){return LoggingMonad(f(x), message);};
 };
 
+// Derive writer versions of the simple functions
+auto writerRoot = Writer(Root, "Taking square root");
+auto writerAddOne = Writer(AddOne, "Adding 1");
+auto writerHalf = Writer(Half, "Dividing by 2");
+
+
 int main()
 {
-    auto startingValue = LoggingMonad{5, "Starting with 5"};
-    auto writerRoot = Writer(Root, "Taking square root");
-    auto writerAddOne = Writer(AddOne, "Adding 1");
-    auto writerHalf = Writer(Half, "Dividing by 2");    
-
-    auto result = startingValue >> writerRoot >> writerAddOne >> writerHalf;
-    
-    cout << result;
+    auto result = LoggingMonad{5, "Starting with 5"} >> writerRoot >> writerAddOne >> writerHalf;
+    cout << result.Log << "\nResult: " << result.Value;
 }
