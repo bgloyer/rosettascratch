a3c783544591e31310fb1e3fbbdbadeb335bc8ae
brian gloyer <brian.gloyer@gmail.com>
2021-06-29 20:43:14 -0700

    simplified

--- writer-monad.cpp
+++ writer-monad.cpp
@@ -4,65 +4,37 @@
 
 using namespace std;
 
-class LoggingMonad
+struct LoggingMonad
 {
-    double m_value;
-    string m_log;
-    
-public:
-    LoggingMonad(double value, string log) :
-    m_value(value), m_log(move(log))
-    {}
-    
-    double Value() const
-    {
-        return m_value;
-    }
-    
-    string Log() const
-    {
-        return m_log;
-    }
+    double Value;
+    string Log;
 };
 
-    ostream& operator<<(ostream &os, const LoggingMonad &monad)
-    {
-        os << monad.Log() << "\nResult: " << monad.Value();
-        return os;
-    }
-
+ostream& operator<<(ostream &os, const LoggingMonad &monad)
+{
+    os << monad.Log << "\nResult: " << monad.Value;
+    return os;
+}
 
 // Use the >> operator as the bind function
 auto operator>>(const LoggingMonad& monad, auto f)
 {
-    auto result = f(monad.Value());
-    return LoggingMonad(result.Value(), monad.Log() + "\n" + result.Log());
+    auto result = f(monad.Value);
+    return LoggingMonad{result.Value, monad.Log + "\n" + result.Log};
 }
 
-double Root(double d)
+auto Root = [](double x){ return sqrt(x); };
+auto AddOne = [](double x){ return x + 1; };
+auto Half = [](double x){ return x / 2.0; };
+
+auto Writer = [](auto f, string message)
 {
-    return sqrt(d);
-}
-
-double AddOne(double d)
-{
-    return d + 1;
-}
-
-double Half(double d)
-{
-    return d / 2.0;
-}
-
-auto Writer(auto f, string message)
-{
-    return [=](double d){return LoggingMonad(f(d), message);};
-}
-
+    return [=](double x){return LoggingMonad(f(x), message);};
+};
 
 int main()
 {
-    auto startingValue = LoggingMonad(5, "Starting with 5");
+    auto startingValue = LoggingMonad{5, "Starting with 5"};
     auto writerRoot = Writer(Root, "Taking square root");
     auto writerAddOne = Writer(AddOne, "Adding 1");
     auto writerHalf = Writer(Half, "Dividing by 2");    
