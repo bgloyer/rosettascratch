c7c2dcdb28d46c98a20245652d65f3d28abbb747
brian gloyer <brian.gloyer@gmail.com>
2021-07-11 13:33:59 -0700

    writer clean-up

--- writer-monad.cpp
+++ writer-monad.cpp
@@ -24,19 +24,20 @@ auto AddOne = [](double x){ return x + 1; };
 auto Half = [](double x){ return x / 2.0; };
 
 // Define a function to create writer monads from the simple functions
-auto Writer = [](auto f, string message)
+auto MakeWriter = [](auto f, string message)
 {
     return [=](double x){return LoggingMonad(f(x), message);};
 };
 
 // Derive writer versions of the simple functions
-auto writerRoot = Writer(Root, "Taking square root");
-auto writerAddOne = Writer(AddOne, "Adding 1");
-auto writerHalf = Writer(Half, "Dividing by 2");
+auto writerRoot = MakeWriter(Root, "Taking square root");
+auto writerAddOne = MakeWriter(AddOne, "Adding 1");
+auto writerHalf = MakeWriter(Half, "Dividing by 2");
 
 
-int main()
+int mainlm()
 {
+    // Compose the writers to compute the golden ratio
     auto result = LoggingMonad{5, "Starting with 5"} >> writerRoot >> writerAddOne >> writerHalf;
     cout << result.Log << "\nResult: " << result.Value;
 }
