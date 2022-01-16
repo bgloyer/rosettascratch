ac4c4e448f37ffd82992072dcb82b576c8cf3b6a
brian gloyer <brian.gloyer@gmail.com>
2022-01-15 22:12:58 -0800

    least-square

---
+++ least-square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ranges>
+
+int main()
+{
+    const int maxSquare = 1000;
+    auto squareCheck = [maxSquare](int i){return 2 * i - 1 > maxSquare;};
+    auto answer = std::views::iota(1) |  // {1, 2, 3, 5, ....}
+      std::views::filter(squareCheck) |  // filter out the ones that are below 1000
+      std::views::take(1);               // take the first one
+    std::cout << answer.front() << '\n';
+}
