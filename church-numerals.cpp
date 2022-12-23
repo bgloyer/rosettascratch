caee415aea596d5125598596d0f47b28344f9a7a
brian gloyer <brian.gloyer@gmail.com>
2022-12-22 23:43:42 -0800

    compiles forever gcc 11.3.1

--- church-numerals.cpp
+++ church-numerals.cpp
@@ -67,6 +67,39 @@ auto Subtract(auto b, auto a) {
     };
 }
 
+//public static Church IsZero(this Church n) => n(_ => ChurchZero)(ChurchOne);
+auto IsZero(auto a)
+{
+    return a([=](auto){return Zero();})(Successor(Zero()));
+}
+
+namespace
+{
+    // static Church looper(this Church v, Church d) =>
+    //     v(_ => v.divr(d).Successor())(ChurchZero);
+    // static Church divr(this Church n, Church d) =>
+    //     n.Subtract(d).looper(d);
+
+    auto Divr(auto n, auto d);
+
+    auto Looper(auto v, auto d)
+    {
+        return IsZero(v)([=](auto){return Successor(Divr(v, d));})(Zero());
+    }
+
+    auto Divr(auto n, auto d)
+    {
+        return Looper(Subtract(n, d), d);
+    }
+}
+
+    // public static Church Divide(this Church dvdnd, Church dvsr) =>
+    //     (dvdnd.Successor()).divr(dvsr);
+
+    auto Divide(auto a, auto b)
+    {
+        return Divr(Successor(a), b);
+    }
 
 //    public static Church Predecessor(this Church n) =>
 
@@ -96,13 +129,16 @@ int main() {
     std::cout << "\n 6 + 9 = " << ToInt(SlowAdd(six, nine));
     std::cout << "\n 9 - 6 = " << ToInt(Subtract(nine, six));
     std::cout << "\n 3 * 4 = " << ToInt(Multiply(three, four));
-    std::cout << "\n 4 * 3 = " << ToInt(Multiply(four, three));
+    std::cout << "\n zzzz4 * 3 = " << ToInt(IsZero(zero)(three));
+    std::cout << "\n zzzz4 * 3 = " << ToInt(IsZero(three)(six));
+    std::cout << "\n 6 / 3 = " << ToInt(Divide(zero, three));
+//    std::cout << "\n 6 / 4 = " << ToInt(Divide(six, four));
     std::cout << "\n 3^4 = " << ToInt(Exp(three, four));
     std::cout << "\n 4^3 = " << ToInt(Exp(four, three));
     std::cout << "\n 0^0 = " << ToInt(Exp(zero, zero));
-    auto looloolooo = Add(Exp(ten, nine), Add(Exp(ten, six), Exp(ten, three)));
-    auto looloolool = Successor(looloolooo);
+    // auto looloolooo = Add(Exp(ten, nine), Add(Exp(ten, six), Exp(ten, three)));
+    // auto looloolool = Successor(looloolooo);
     std::cout << "\n 10^(3-1) -1  " << ToInt(Predecessor(Exp(ten, Predecessor(three))));
 
-    std::cout << "\n 10^9 + 10^6 + 10^3 + 1 = " << ToInt(looloolool) << "\n";
+//    std::cout << "\n 10^9 + 10^6 + 10^3 + 1 = " << ToInt(looloolool) << "\n";
 }
