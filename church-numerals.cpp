03985aef126f5347c93effce777afa23d9f5f922
brian gloyer <brian.gloyer@gmail.com>
2022-12-23 09:47:36 -0800

    church subtraction/division

--- church-numerals.cpp
+++ church-numerals.cpp
@@ -8,10 +8,10 @@ auto Zero() {
 }
 
 // apply the function f one more time
-auto Successor(auto church) {
+auto Successor(auto a) {
     return [=](auto f) {
         return [=](auto x) {
-            return church(f)(f(x));
+            return a(f)(f(x));
         };
     };
 }
@@ -22,13 +22,6 @@ auto Add(auto a, auto b) {
         return [=](auto x) {
             return a(f)(b(f)(x));
         };
-    };
-}
-
-// apply the Successor function a times to b
-auto SlowAdd(auto a, auto b) {
-    {
-        return a([](auto c){ return Successor(c);})(b);
     };
 }
 
@@ -45,63 +38,51 @@ auto Exp(auto a, auto b) {
 }
 
 // apply the function f one less time
-// f => x => n(g => h => h(g(f)) )(_ => x)(a => a);
-auto Predecessor(auto n) {
+auto Predecessor(auto a) {
     return [=](auto f) {
         return [=](auto x) {
-            return n(
+            return a(
                 [=](auto g) {
                     return [=](auto h){
                         return h(g(f));
                     };
                 }
-             )([=](auto) {return x;})([](auto a){return a;});
+             )([=](auto) {return x;})([](auto y){return y;});
         };
     };
 }
 
-// apply the Successor function a times to b
-auto Subtract(auto b, auto a) {
+// apply the Predecessor function b times to a
+auto Subtract(auto a, auto b) {
     {
-        return a([](auto c){ return Predecessor(c);})(b);
-    };
-}
+        auto a_minus_b = b([](auto c){ return Predecessor(c);})(a);
 
-//public static Church IsZero(this Church n) => n(_ => ChurchZero)(ChurchOne);
-auto IsZero(auto a)
-{
-    return a([=](auto){return Zero();})(Successor(Zero()));
+        // Each lambda has its own type which gives '3-1' a different
+        // type than '4-2'.  Normalize the number based on successors to 0.  
+        return a_minus_b([=](auto f) {return Successor(f);})(Zero());
+    };
 }
 
 namespace
 {
-    // static Church looper(this Church v, Church d) =>
-    //     v(_ => v.divr(d).Successor())(ChurchZero);
-    // static Church divr(this Church n, Church d) =>
-    //     n.Subtract(d).looper(d);
+    // helper functions for division.  These funtions are only
+    // visible in this source file
 
-    auto Divr(auto n, auto d);
-
-    auto Looper(auto v, auto d)
-    {
-        return IsZero(v)([=](auto){return Successor(Divr(v, d));})(Zero());
+    // end the recusrion
+    auto Divr(decltype(Zero()), auto) {
+        return Zero();
     }
 
-    auto Divr(auto n, auto d)
-    {
-        return Looper(Subtract(n, d), d);
+    // count how many times b can be subtracted from a
+    auto Divr(auto a, auto b) {
+        return Successor(Divr(Subtract(a, b), b));
     }
 }
 
-    // public static Church Divide(this Church dvdnd, Church dvsr) =>
-    //     (dvdnd.Successor()).divr(dvsr);
-
-    auto Divide(auto a, auto b)
-    {
-        return Divr(Successor(a), b);
-    }
-
-//    public static Church Predecessor(this Church n) =>
+// apply the function a / b times
+auto Divide(auto a, auto b) {
+    return Divr(Subtract(Successor(a), b), b);
+}
 
 // create a Church numeral from an integer at compile time
 template <int N> constexpr auto ToChurch() {
@@ -115,30 +96,26 @@ int ToInt(auto church) {
 }
 
 int main() {
-
+    // show some examples
     auto zero = Zero();
     auto three = Successor(Successor(Successor(zero)));
     auto four = Successor(three);
     auto six = ToChurch<6>();
     auto nine = ToChurch<9>();
     auto ten = Successor(nine);
-    
-    std::cout << "\n Pred(3) = " << ToInt(Predecessor(three));
+
     std::cout << "\n 3 + 4 = " << ToInt(Add(three, four));
     std::cout << "\n 4 + 3 = " << ToInt(Add(four, three));
-    std::cout << "\n 6 + 9 = " << ToInt(SlowAdd(six, nine));
-    std::cout << "\n 9 - 6 = " << ToInt(Subtract(nine, six));
     std::cout << "\n 3 * 4 = " << ToInt(Multiply(three, four));
-    std::cout << "\n zzzz4 * 3 = " << ToInt(IsZero(zero)(three));
-    std::cout << "\n zzzz4 * 3 = " << ToInt(IsZero(three)(six));
-    std::cout << "\n 6 / 3 = " << ToInt(Divide(zero, three));
-//    std::cout << "\n 6 / 4 = " << ToInt(Divide(six, four));
+    std::cout << "\n 4 * 3 = " << ToInt(Multiply(four, three));
     std::cout << "\n 3^4 = " << ToInt(Exp(three, four));
     std::cout << "\n 4^3 = " << ToInt(Exp(four, three));
     std::cout << "\n 0^0 = " << ToInt(Exp(zero, zero));
-    // auto looloolooo = Add(Exp(ten, nine), Add(Exp(ten, six), Exp(ten, three)));
-    // auto looloolool = Successor(looloolooo);
-    std::cout << "\n 10^(3-1) -1  " << ToInt(Predecessor(Exp(ten, Predecessor(three))));
-
-//    std::cout << "\n 10^9 + 10^6 + 10^3 + 1 = " << ToInt(looloolool) << "\n";
+    std::cout << "\n 9 - 6 = " << ToInt(Subtract(nine, six));
+    std::cout << "\n 9 - 9 = " << ToInt(Subtract(nine, nine));
+    std::cout << "\n 6 / 3 = " << ToInt(Divide(six, three));
+    std::cout << "\n 3 / 6 = " << ToInt(Divide(three, six));
+    auto looloolooo = Add(Exp(ten, nine), Add(Exp(ten, six), Exp(ten, three)));
+    auto looloolool = Successor(looloolooo);
+    std::cout << "\n 10^9 + 10^6 + 10^3 + 1 = " << ToInt(looloolool) << "\n";
 }
